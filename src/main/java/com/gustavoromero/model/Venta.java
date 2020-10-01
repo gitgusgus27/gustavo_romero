e061307b3f72540bffdc3e1a6b2e8f161ba98019
gitgusgus27 <72162281+gitgusgus27@users.noreply.github.com>
2020-10-01 17:54:48 -0300

    tarea 1 gustavo romero
    primera tarea gustavo romero

---
+++ src/main/java/com/gustavoromero/model/Venta.java
@@ -0,0 +1,82 @@
+package com.gustavoromero.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "venta")
+public class Venta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_Venta;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "id_Vendedor", nullable = false, foreignKey = @ForeignKey(name = "FK_id_Vendedor"))
+	private Vendedor vendedor;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "id_Cliente", nullable = false, foreignKey = @ForeignKey(name = "FK_id_Cliente"))
+	private Cliente cliente;
+	
+	
+	@Column(name = "costo", nullable = false)
+	private float costo;
+	
+	private LocalDateTime fecha_Venta;
+
+	public int getId_Venta() {
+		return id_Venta;
+	}
+
+	public void setId_Venta(int id_Venta) {
+		this.id_Venta = id_Venta;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public float getCosto() {
+		return costo;
+	}
+
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+
+	public LocalDateTime getFecha_Venta() {
+		return fecha_Venta;
+	}
+
+	public void setFecha_Venta(LocalDateTime fecha_Venta) {
+		this.fecha_Venta = fecha_Venta;
+	}
+	
+    
+}
